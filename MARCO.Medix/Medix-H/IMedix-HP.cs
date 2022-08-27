c9d881608f9e9be52ece7c6da0931a87af5e54c3
Younes <Younes@DESKTOP-F5DKJ59>
2022-08-27 13:28:00 +0430

    GetHCode added to Medix-HP

--- MARCO.Medix/Medix-H/IMedix-HP.cs
+++ MARCO.Medix/Medix-H/IMedix-HP.cs
@@ -1,4 +1,6 @@
 ﻿using MARCO.Medix.Grpc;
+using MARCO.Medix.Medix_H.Dtos.Request;
+using MARCO.Medix.Medix_H.Dtos.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,9 @@ using System.Threading.Tasks;
 namespace MARCO.Medix.Medix_H
 {
     [ServiceContract]
-    public interface IMedix_HP:IGrpcService
+    public interface IMedix_HP : IGrpcService
     {
+
+        public IHCodeResponse GetHCode(IHCodeRequest codeRequest);
     }
 }
