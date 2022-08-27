dbe2708f032712b73f72f54454cd2f640c407bb4
Younes <Younes@DESKTOP-F5DKJ59>
2022-08-27 10:29:46 +0430

    IMedix-HP service

---
+++ MARCO.Medix/Medix-H/IMedix-HP.cs
@@ -0,0 +1,15 @@
+﻿using MARCO.Medix.Grpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARCO.Medix.Medix_H
+{
+    [ServiceContract]
+    public interface IMedix_HP:IGrpcService
+    {
+    }
+}
