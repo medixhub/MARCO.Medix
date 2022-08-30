a9505caf88d8b30ef0417dd95f939eb8bdf0b013
Younes <Younes@DESKTOP-F5DKJ59>
2022-08-30 10:45:54 +0430

    MedixHU & MedixNU

---
+++ MARCO.Medix/Medix-H/IMedixHU.cs
@@ -0,0 +1,18 @@
+﻿using MARCO.Medix.Medix_H.Dtos.Request;
+using MARCO.Medix.Medix_H.Dtos.Response;
+using MARCO.Medix.Medix_H.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARCO.Medix.Medix_H
+{
+    public interface IMedixHU
+    {
+        public IHCodeResponse GetHCode(IHCodeRequest codeRequest);
+        public IAsyncEnumerable<IPhysician> GetPhysicians(string query);
+    }
+}
---
+++ MARCO.Medix/Medix-N/IMedixNU.cs
@@ -0,0 +1,15 @@
+﻿using MARCO.Medix.Grpc.Dtos;
+using MARCO.Medix.Medix_N.Dtos.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARCO.Medix.Medix_N
+{
+    public interface IMedixNU
+    {
+        public IBaseResponse DownloadStudy(IDownloadStudyRequest downloadStudyRequest);
+    }
+}
