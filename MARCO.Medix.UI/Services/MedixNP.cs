b81d4341b5628cd6a311eb5ffa7aefeb5c09762d
younes ganjvar <ahora.xsmart@yahoo.com>
2022-09-03 11:21:52 +0430

    MedixNP

---
+++ MARCO.Medix.UI/Services/MedixNP.cs
@@ -0,0 +1,16 @@
+﻿using MARCO.Medix.Grpc.Dtos;
+using MARCO.Medix.Medix_N;
+using MARCO.Medix.Medix_N.Dtos.Request;
+
+namespace MARCO.Medix.UI.Services
+{
+    public class MedixNP : IMedixNP
+    {
+        public IHttpContextAccessor HttpContextAccessor => throw new NotImplementedException();
+
+        public IBaseResponse DownloadStudy(IDownloadStudyRequest downloadStudyRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
